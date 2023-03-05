d787af14170d269f4155f83e276eabbe7ab06a4c
Michl <rauscher_michael92@gmx.de>
2023-03-05 17:47:02 +0100

    New Table strucutre with filter and sorting

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import SmartTable from 'vuejs-smart-table'
+
 
 import './assets/main.css'
 
 const app = createApp(App)
 
+app.use(SmartTable)
 app.use(router)
+
 
 app.mount('#app')
--- src/views/Co2TableView.vue
+++ src/views/Co2TableView.vue
@@ -1,55 +1,64 @@
 <script setup>
-  import { watch, ref} from "vue"
+  import {ref} from "vue"
   import { onMounted } from "vue"
-  import axios from "axios";
+  import axios from "axios"
+
+  const companies = []
+  const companiesdata = ref(companies)
 
   onMounted(async () => {
     await axios
         .get('http://localhost:3000/companies')
         .then(response => {
-          companiesdata.value = companies.value
           companies.value = response.data
   
         })
         companiesdata.value = companies.value
   })
+</script>
 
-  const companies = []
-  const companiesdata = ref(companies)
-  const search = ref("")
-
-  watch(search, () => {
-    companiesdata.value = companies.value.filter(company => company.Land.toLowerCase().includes(search.value.toLowerCase()))
-  })
+<script>
+export default {
+    name: 'TheBasics',
+    data: () => ({
+      filters: {
+        Land: { value: '', keys: ['Land'] },
+        Age: { value: '', keys: ['Age'] },
+        Email: { value: '', keys: ['Email'] },
+        MtCO2year: { value: '', keys: ['MtCO2year'] }
+      }
+    }),
+    methods: {
+      co2Sort (row) {
+        return parseFloat(row.MtCO2year)
+      }
+    }
+}
 </script>
 
 <template>
-  <main>
-    <h1>CO2 Table view</h1>
-    <input v-model.trim="search" type="text" placeholder="Search for Land...">
-    <table width ="50%" border ="4">
-    <caption>Titel</caption>
-      <thead>
-        <tr>
-          <th>Land</th>
-          <th>Rang</th> 
-          <th>Unternehmen</th>
-          <th>MtCO2/year</th>
-        </tr>
-      </thead>
-      <!-- itterate through the companies reseved from the GET request -->
-      <tbody>
-        <tr v-for="company in companiesdata" :key="company.Rang" :company="company">
-          <td>{{ company.Land }}</td>
-          <td>{{ company.Rang }}</td>
-          <td>{{ company.Unternehmen }}</td>
-          <td>{{ company["MtCO2/year"] }}</td>
-        </tr>
-      </tbody>
-    </table>
-  </main>
+    <main>
+      <h1>CO2 Table view</h1>
+        <label>Filter by Name:</label>
+        <input v-model="filters.Land.value"/>
+        <VTable 
+        :data="companiesdata"
+        :filters="filters"
+        >
+          <template #head>
+              <VTh sortKey="Land">Land</VTh>
+              <VTh sortKey="Rang">Rang</VTh>
+              <VTh sortKey="Unternehmen">Unternehmen</VTh>
+              <VTh :sortKey="co2Sort">MtCO2/year</VTh>
+          </template>
+          <template #body="{rows}">
+            <tr v-for="row in rows" :key="row.id">
+              <td>{{ row.Land }}</td>
+              <td>{{ row.Rang }}</td>
+              <td>{{ row.Unternehmen }}</td>
+              <td>{{ row.MtCO2year }}</td>
+            </tr>
+          </template>
+        </VTable>
+    </main>
 </template>
-
-<style scoped>
-
-</style>
